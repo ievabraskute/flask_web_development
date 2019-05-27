9d4413dab9f2f06968d7ee6559951c128e199134
ievabraskute <i.braskute@gmail.com>
2019-05-27 19:58:01 +0300

    accept user input through web forms

--- my_app.py
+++ my_app.py
@@ -1,48 +1,45 @@
-from flask import Flask
-from flask import make_response
-from flask import redirect
-from flask import abort
-from flask import render_template
-from flask_bootstrap import Bootstrap
-from flask_moment import Moment
 from datetime import datetime
 
+from flask import Flask, flash, redirect, render_template, session, url_for
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my secret key'
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name!=form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
     return render_template(
-        'index.html',
-        current_time=datetime.utcnow()
-    )
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+        "index.html",
+        current_time=datetime.utcnow(),
+        name=session.get('name'),
+        form=form)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
-
-
-@app.route('/get_response')
-def get_response():
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('answer', '42')
-    return response
-
-
-@app.route('/redirect')
-def my_redirect():
-    return redirect('http://www.15min.lt')
+    return render_template("500.html"), 500
